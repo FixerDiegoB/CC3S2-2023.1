b98019258e596e2fa39211fc59dffafd344ece2e
FixerDiego <57854488+FixerDiegoB@users.noreply.github.com>
2023-05-15 12:43:46 -0500

    Pregunta2

--- ExamenParcial-3S2/src/main/java/Pregunta2/TicTacToe.java
+++ ExamenParcial-3S2/src/main/java/Pregunta2/TicTacToe.java
@@ -2,11 +2,21 @@ package Pregunta2;
 
 public class TicTacToe {
     int[][] tablero = new int[3][3];
+    char actualJugador = 'X';
 
     public void jugar(int x, int y) {
         validarLimitesTablero(x, y);
         validarLugarOcupado(x, y);
         marcarCasilla(x, y);
+        if (obtenerGanador() != ' ') {
+            System.out.println("El ganador es " + actualJugador);
+            return;
+        }
+        else if (hayEmpate()) {
+            System.out.println("Hay empate");
+            return;
+        }
+        cambiarTurno();
     }
     private void validarLimitesTablero(int x, int y) {
         if (x < 1 || x > 3) {
@@ -22,7 +32,63 @@ public class TicTacToe {
         }
     }
     private void marcarCasilla(int x, int y) {
-        tablero[x - 1][y - 1] = 1;
+        if (actualJugador == 'X')
+            tablero[x - 1][y - 1] = 1;
+        else
+            tablero[x - 1][y - 1] = 2;
     }
-
+    private  void cambiarTurno() {
+        if (actualJugador == 'X')
+            actualJugador = 'O';
+        else
+            actualJugador = 'X';
+    }
+    public char proximoJugador() {
+        return actualJugador;
+    }
+    public char obtenerGanador() {
+        if (hayGanadorPorLineaHorizontal() || hayGanadorPorLineaVertical() ||
+                hayGanadorPorPrimeraLineaDiagonal() || hayGanadorPorSegundaLineaDiagonal())
+            return actualJugador;
+        return ' ';
+    }
+    private boolean hayGanadorPorLineaHorizontal() {
+        for (int i = 0; i < 3; i++) {
+            int valor = tablero[i][0];
+            if (valor != 0 && tablero[i][1] == valor && tablero[i][2] == valor)
+                return true;
+        }
+        return false;
+    }
+    private boolean hayGanadorPorLineaVertical() {
+        for (int i = 0; i < 3; i++) {
+            int valor = tablero[0][i];
+            if (valor != 0 && tablero[1][i] == valor && tablero[2][i] == valor)
+                return true;
+        }
+        return false;
+    }
+    private boolean hayGanadorPorPrimeraLineaDiagonal() {
+        int valor = tablero[0][0];
+        for (int i = 1; i < 3; i++) {
+            if (tablero[i][i] != valor)
+                return false;
+        }
+        return valor != 0;
+    }
+    private boolean hayGanadorPorSegundaLineaDiagonal() {
+        int valor = tablero[0][2];
+        for (int i = 1; i < 3; i++) {
+            if (tablero[i][2 - i] != valor)
+                return false;
+        }
+        return valor != 0;
+    }
+    public boolean hayEmpate() {
+        for (int i = 0; i < 3; i++)
+            for (int j = 0; j< 3; j++)
+                if (tablero[i][j] == 0)
+                    return false;
+        return obtenerGanador() == ' ';
+    }
 }
--- ExamenParcial-3S2/src/main/java/Pregunta2/TicTacToeTest.java
+++ ExamenParcial-3S2/src/main/java/Pregunta2/TicTacToeTest.java
@@ -1,13 +1,14 @@
 package Pregunta2;
 
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 
 public class TicTacToeTest {
-    static TicTacToe ticTacToe;
-    @BeforeAll
-    public static void setUp() {
+    TicTacToe ticTacToe;
+    @BeforeEach
+    public void setUp() {
         ticTacToe = new TicTacToe();
     }
     @Test
@@ -30,5 +31,75 @@ public class TicTacToeTest {
         Assertions.assertThrows(RuntimeException.class, () -> {
             ticTacToe.jugar(2, 2);
         });
+    }
+
+    @Test
+    public void juegaXPrimero() {
+        Assertions.assertEquals('X', ticTacToe.proximoJugador());
+    }
+
+    @Test
+    public void juegaODespuesDeX() {
+        ticTacToe.jugar(2, 2);
+        Assertions.assertEquals('O', ticTacToe.proximoJugador());
+    }
+
+    @Test
+    public void noHayGanador() {
+        Assertions.assertEquals(' ', ticTacToe.obtenerGanador());
+    }
+
+    @Test
+    public void ganadorPorLineaHorizontal() {
+        ticTacToe.jugar(1, 1);
+        ticTacToe.jugar(2, 1);
+        ticTacToe.jugar(1, 2);
+        ticTacToe.jugar(2, 2);
+        ticTacToe.jugar(1, 3);
+        Assertions.assertEquals('X', ticTacToe.obtenerGanador());
+    }
+
+    @Test
+    public void ganadorPorLineaVertical() {
+        ticTacToe.jugar(1, 1);
+        ticTacToe.jugar(1, 2);
+        ticTacToe.jugar(2, 1);
+        ticTacToe.jugar(2, 2);
+        ticTacToe.jugar(3, 1);
+        Assertions.assertEquals('X', ticTacToe.obtenerGanador());
+    }
+
+    @Test
+    public void ganadorPorPrimeraLineaDiagonal() {
+        ticTacToe.jugar(1, 1);
+        ticTacToe.jugar(1, 2);
+        ticTacToe.jugar(2, 2);
+        ticTacToe.jugar(2, 1);
+        ticTacToe.jugar(3, 3);
+        Assertions.assertEquals('X', ticTacToe.obtenerGanador());
+    }
+
+    @Test
+    public void ganadorPorSegundaLineaDiagonal() {
+        ticTacToe.jugar(1, 3);
+        ticTacToe.jugar(1, 2);
+        ticTacToe.jugar(2, 2);
+        ticTacToe.jugar(2, 1);
+        ticTacToe.jugar(3, 1);
+        Assertions.assertEquals('X', ticTacToe.obtenerGanador());
+    }
+
+    @Test
+    public void empate(){
+        ticTacToe.jugar(1, 1);
+        ticTacToe.jugar(1, 2);
+        ticTacToe.jugar(1, 3);
+        ticTacToe.jugar(2, 1);
+        ticTacToe.jugar(2, 3);
+        ticTacToe.jugar(2, 2);
+        ticTacToe.jugar(3, 1);
+        ticTacToe.jugar(3, 3);
+        ticTacToe.jugar(3, 2);
+        Assertions.assertTrue(ticTacToe.hayEmpate());
     }
 }
