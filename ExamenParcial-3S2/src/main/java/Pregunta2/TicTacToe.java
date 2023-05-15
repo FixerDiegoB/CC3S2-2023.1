b9852afc9ba3f1b967c786f52a622f70a3f5e0e9
FixerDiego <57854488+FixerDiegoB@users.noreply.github.com>
2023-05-15 11:11:08 -0500

    Requisito1

--- ExamenParcial-3S2/src/main/java/Pregunta2/TicTacToe.java
+++ ExamenParcial-3S2/src/main/java/Pregunta2/TicTacToe.java
@@ -1,9 +1,28 @@
 package Pregunta2;
 
 public class TicTacToe {
+    int[][] tablero = new int[3][3];
+
     public void jugar(int x, int y) {
+        validarLimitesTablero(x, y);
+        validarLugarOcupado(x, y);
+        marcarCasilla(x, y);
+    }
+    private void validarLimitesTablero(int x, int y) {
         if (x < 1 || x > 3) {
-            throw new RuntimeException("X esta fuera del tablero.");
+            throw new RuntimeException("X está fuera del tablero.");
+        }
+        if (y < 1 || y > 3) {
+            throw new RuntimeException("Y está fuera del tablero.");
         }
     }
+    private void validarLugarOcupado(int x, int y) {
+        if (tablero[x - 1][y - 1] == 1) {
+            throw new RuntimeException("Lugar ocupado.");
+        }
+    }
+    private void marcarCasilla(int x, int y) {
+        tablero[x - 1][y - 1] = 1;
+    }
+
 }
--- ExamenParcial-3S2/src/main/java/Pregunta2/TicTacToeTest.java
+++ ExamenParcial-3S2/src/main/java/Pregunta2/TicTacToeTest.java
@@ -1,4 +1,34 @@
 package Pregunta2;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
 public class TicTacToeTest {
+    static TicTacToe ticTacToe;
+    @BeforeAll
+    public static void setUp() {
+        ticTacToe = new TicTacToe();
+    }
+    @Test
+    public void limiteX() {
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            ticTacToe.jugar(5, 2);
+        });
+    }
+
+    @Test
+    public void limiteY() {
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            ticTacToe.jugar(2, 5);
+        });
+    }
+
+    @Test
+    public void lugarOcupado() {
+        ticTacToe.jugar(2, 2);
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            ticTacToe.jugar(2, 2);
+        });
+    }
 }
